dc633d483e934c0cb771951b5f3ef86a8b48e363
DagobertoCadilhe <dagoberto.ccadilhe@gmail.com>
2026-06-09 07:41:22 -0300

    feat: player-service e api-gateway configurados

--- player-service/src/main/java/com/marketguild/player_service/controller/PlayerController.java
+++ player-service/src/main/java/com/marketguild/player_service/controller/PlayerController.java
@@ -1,9 +1,10 @@
 package com.marketguild.player_service.controller;
 
+import com.marketguild.player_service.dto.PlayerRequest;
+import com.marketguild.player_service.model.Player;
+import com.marketguild.player_service.repository.PlayerRepository;
 import com.marketguild.player_service.service.PlayerService;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/players")
@@ -15,4 +16,22 @@ public class PlayerController {
     }
 
 
+
+    @PostMapping()
+    public Player createPlayer(@RequestBody PlayerRequest playerDTO){
+
+        Player newPlayer = playerService.createPlayer(playerDTO.getName(), playerDTO.getBalance());
+
+        return newPlayer;
+    }
+
+    @GetMapping("/{id}")
+    public Player findPlayerById(@PathVariable Long id){
+        return playerService.findPlayerById(id);
+    }
+
+    @PatchMapping("/{id}/balance")
+    public void updateBalance(@PathVariable Long id, @RequestBody Double price){
+        playerService.updateBalance(id, price);
+    }
 }
---
+++ player-service/src/main/java/com/marketguild/player_service/dto/PlayerRequest.java
@@ -0,0 +1,22 @@
+package com.marketguild.player_service.dto;
+
+public class PlayerRequest {
+    private String name;
+    private Double balance;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+}
